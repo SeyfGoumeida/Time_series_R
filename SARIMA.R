d469c06eae6a7f0c2770fa208bc9c920cad32f3f
GOUMEIDA AHMED SEYFEDDINE <36012779+SeyfGoumeida@users.noreply.github.com>
2021-12-17 11:59:20 +0100

    Update SARIMA.R

--- SARIMA.R
+++ SARIMA.R
@@ -1,13 +1,25 @@
 install.packages("fpp")
 library(fpp)
-euretail_timeserie =ts(euretail)
-plot(euretail_timeserie)
-#stationarity = Non 
-compose(euretail_timeserie)
+
+
+data("euretail")
+plot(euretail,ylab="Retail index",xlab="Years")
 euretail_timeserie_components = decompose(birthstimeseries)
 plot(euretail_timeserie_components)
-# For monthly data, in which there are 12 periods in a season, the seasonal difference of Y at period t is Y(t)-Y(t-12)
+tsdisplay(euretail)
+tsdisplay(diff(euretail,4))
+tsdisplay(diff(diff(euretail,4)))
+
+fit<- Arima(euretail,order =c(1,1,0),seasonal = c(1,1,0))
+tsdisplay(residuals(fit))
+Box.test(residuals(fit),lag=16,type="Ljung")
+
+
+fit3<- Arima(euretail,order =c(0,1,3),seasonal = c(0,1,1))
+tsdisplay(residuals(fit3))
+Box.test(residuals(fit3),lag=16,type="Ljung")
+
 # Change plot size to 8 x 3
 options(repr.plot.width=20, repr.plot.height=6)
-plot(euretail_timeserie)
+
 plot(acf(euretail_timeserie))
